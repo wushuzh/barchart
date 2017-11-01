8c43bfb458cd75db31501a177d14ae949d237354
wushuzh <zhangjiaqiang@gmail.com>
2017-11-01 15:22:17 +0800

    simulate bugs and costs data for n days

--- barchart/app.py
+++ barchart/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 
 
 app = Flask(__name__)
@@ -8,6 +9,15 @@ app = Flask(__name__)
 def cahrt(bars_count):
     if bars_count <= 0:
         bars_count = 1
+
+    data = {"days": [], "bugs": [], "costs": []}
+    for i in range(1, bars_count + 1):
+        data['days'].append(i)
+        data['bugs'].append(random.randint(1, 100))
+        data['costs'].append(random.uniform(1.00, 1000.00))
+
+    print(data)
+
     return render_template("chart.html", bars_count=bars_count)
 
 
